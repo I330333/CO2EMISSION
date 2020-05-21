c6d91231c31090e9fe806cf21e80b8007181afb3
I330333 <59384874+I330333@users.noreply.github.com>
2020-05-21 18:47:23 +0530

    Commit for Co2emission

---
+++ co2emis/src/main/java/com/inter/co2emis/Co2emisApplication.java
@@ -0,0 +1,13 @@
+package com.inter.co2emis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Co2emisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Co2emisApplication.class, args);
+	}
+
+}
---
+++ co2emis/src/test/java/com/inter/co2emis/Co2emisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inter.co2emis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Co2emisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
